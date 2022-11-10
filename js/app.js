8e1e488af426ac6276b12e91a2e06626ae221d79
Md Mahmudunnobe <mahmud.nobe@edu.minerva.edu>
2022-11-10 00:47:28 -0500

    add the filter functionality

--- js/app.js
+++ js/app.js
@@ -1,27 +1,44 @@
 // import the data
-//const tableData = data;
-
-//console.log(tableData);
+const tableData = data;
 
 // connect with the table <tbody> in HTML
 var tbody = d3.select('tbody');
-//console.log(tbody);
 
-//console.log(tbody);
-//console.log(data);
+
 function buildTable(data) {
     tbody.html("");
-    data.forEach(function(row) {
-        //var tbody = d3.select('tbody');
-        var trow = tbody.append('tr');
-        Object.values(row).forEach(function(data) {
-            cell = trow.append('td');
-            cell.text(data);
+    data.forEach(element => {
+        var row = tbody.append('tr');
+        
+        Object.values(element).forEach( value => {
+            var cell = row.append('td');
+            cell.text(value);
         });
-    })
+    });
 }
 
-buildTable(data);
+buildTable(tableData);
+
+// connect with the filer button
+var filter = d3.select("#filter-btn");
+
+
+function handleClick(event) {
+    var date = d3.select('#datetime').property('value');
+    
+    var filteredData = tableData;
+
+    if (date) {
+        filteredData = filteredData.filter(data_row => data_row.datetime == date);
+    }
+
+    buildTable(filteredData);
+
+}
+
+// change the data based on the filter value
+filter.on('click', handleClick);
+
 
 
 
